275b1a15bed1956fc68d1a454b57de2c8d754752
Jefferson Martins <lazumbra@gmail.com>
2023-05-02 16:05:07 -0300

    add the project01

---
+++ projeto-01/main.py
@@ -0,0 +1,13 @@
+# Vídeo: How To Write Unit Tests For Existing Python Code // Part 1 of 2
+from pay.order import LineItem, Order
+from pay.payment import pay_order
+
+def main():
+    # Test card number: 1249190007575059
+    order = Order()
+    order.line_items.append(LineItem(name="Shoes", price=100_00, quantity=2))
+    order.line_items.append(LineItem(name="Hat", price=50_00))
+    pay_order(order)
+
+if __name__ == "__main__":
+    main()
---
+++ projeto-01/pay/payment.py
@@ -0,0 +1,13 @@
+# Vídeo: How To Write Unit Tests For Existing Python Code // Part 1 of 2
+from pay.order import Order
+from pay.processor import PaymentProcessor
+
+def pay_order(order: Order):
+    if order.total == 0:
+        raise ValueError("Can't pay an order with total 0.")
+    card = input("Coloca o nuymero do cartao: ")
+    month = int(input("Coloque o mes do vencimento do cartao: "))
+    year = int(input("Coloque o ano que vence o cartao: "))
+    payment_processor = PaymentProcessor("6cfb67f3-6281-4031-b893-ea85db0dce20")
+    payment_processor.charge(card, month, year, amount=order.total)
+    order.pay()
